e60ae9d1617d0033cca9117bdbf7f6ccafefd619
sarism <saraisabelmayalasso@gmail.com>
2024-03-05 08:04:15 -0500

    subir

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const nombre = prompt("Ingrese su nombre"); 
+
+
+const apellido = prompt("Ingrese su apellido"); 
+
+let BirthYear = prompt("Ingrese su año de nacimiento");
+BirthYear = parseInt(BirthYear)
+
+
+const Age = (2024-BirthYear);
+
+
+const BirthDay = prompt("Ingrese su dia de nacimiento");
+
+
+const BirtMonth = prompt("Ingrese su mes de nacimiento");
+
+
+console.log ("Mi nombre es ", nombre, apellido, "mi edad es ", Age, "años y nací el ", BirthDay, "de ", BirtMonth, BirthYear);
